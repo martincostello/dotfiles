00f279537273f7e6cd59869de9152a3ff9e5edd5
Martin Costello <martin@martincostello.com>
2025-11-26 12:41:36 +0000

    Fix GPG script
    Fix breaking non-signing operations.

--- git-gpg-sign.ps1
+++ git-gpg-sign.ps1
@@ -1,3 +1,12 @@
 #! /usr/bin/env pwsh
-$passphrase = (git config --get gpg.passphrase)
-& gpg --batch --pinentry-mode=loopback --passphrase "${passphrase}" $args
+
+# We want the GnuPG version, not the version bundled with Git
+$gpg = (where.exe gpg | Select-Object -Last 1)
+
+if ($args.Contains("--sign")) {
+    $passphrase = (git config --get gpg.passphrase)
+    & "${gpg}" --batch --pinentry-mode=loopback --passphrase "${passphrase}" $args
+}
+else {
+  & "${gpg}" $args
+}

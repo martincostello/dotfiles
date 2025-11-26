367b71c2f68e8540952e85a56ff4659726f961d7
Martin Costello <martin@martincostello.com>
2025-11-26 16:01:20 +0000

    Fix argument handling
    Handle GitHub Desktop and Git CLI arguments.

--- git-gpg-sign.ps1
+++ git-gpg-sign.ps1
@@ -3,7 +3,7 @@
 # We want the GnuPG version, not the version bundled with Git
 $gpg = (where.exe gpg | Select-Object -Last 1)
 
-if ($args.Contains("--sign")) {
+if (($args.Contains("--sign") -Or $args.Contains("-s")) -Or $args.Contains("-bsau")) {
     $passphrase = (git config --get gpg.passphrase)
     & "${gpg}" --batch --pinentry-mode=loopback --passphrase "${passphrase}" $args
 }
